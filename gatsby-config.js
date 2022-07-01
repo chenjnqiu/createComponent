734f8adee601a3ae4b6e8740e98cf53f4990f481
chenjinqiu <chenjinqiu@hualala.com>
2022-07-01 09:46:32 +0800

    peizhi

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 //gatsby-config.js
 module.exports = {
     plugins: ['gatsby-plugin-less'],
+    flags: {
+        PRESERVE_FILE_DOWNLOAD_CACHE: true,
+        PRESERVE_WEBPACK_CACHE: true
+    },
 }
